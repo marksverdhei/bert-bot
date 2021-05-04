4873cc352c4ac40ab3e671f3464236818416b96a
marksverdhei <marksverdhei@hotmail.com>
2021-05-04 02:22:13 +0200

    Remove stopwords and fix id bug

--- src/cogs/bert.py
+++ src/cogs/bert.py
@@ -5,50 +5,65 @@ from transformers import BertTokenizer, BertForMaskedLM
 import torch
 import functools
 import re
-from cogs.utils import embed_templates
+from cogs.utils import embed_templates, stopwords
 
 
 # SET UP MODELS
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertForMaskedLM.from_pretrained("bert-base-uncased")
 mask_id = tokenizer("[MASK]", add_special_tokens=False).input_ids[0]
+stopword_ids = stopwords.get_stopword_ids(tokenizer, stopwords.english, cased=False)
 model.eval()
 
 nor_tokenizer = BertTokenizer.from_pretrained("ltgoslo/norbert")
 nor_model = BertForMaskedLM.from_pretrained("ltgoslo/norbert")
 nor_mask_id = nor_tokenizer("[MASK]", add_special_tokens=False).input_ids[0]
+nor_stopword_ids = stopwords.get_stopword_ids(nor_tokenizer, stopwords.norwegian, cased=True)
 nor_model.eval()
 # #
 
 
-def get_topn(content, tokenizer, model, mask_id, n, stopwords=False):
+def get_topn(content, tokenizer, model, mask_id, n, stopwords=None):
     content = re.sub("_+", "[MASK]", content)
     tokens = tokenizer(content, return_tensors="pt")
     with torch.no_grad():
         outputs = model(**tokens).logits.squeeze()
     mask_positions, = torch.where(tokens.input_ids[0] == mask_id)
+    print("mask positions:")
+    print(mask_positions)
+    print("Content:")
+    print(content)
+    print(tokenizer.convert_ids_to_tokens(tokens.input_ids[0]))
+    print(tokens.input_ids[0])
 
     if not len(mask_positions):
         yield ""
     else:
         preds = outputs[mask_positions]
         for pred in preds:
-            topn = (-pred).squeeze().argsort()[:n]
+            sorted_logits = (-pred).squeeze().argsort()
+            if stopwords is not None:
+                places_where_stopword_id = (1 - ((sorted_logits == stopwords[:, None]).sum(0))).bool()
+                topn = sorted_logits[places_where_stopword_id][:n]
+            else:
+                topn = sorted_logits[:n]
+
             substitutions = tokenizer.convert_ids_to_tokens(topn)
             for j, s in enumerate(substitutions):
                 yield f"**{s}**"
 
 
-def insert(tokenizer, model, content):
+def insert(tokenizer, model, mask_id, content, stopwords=None):
     return functools.reduce(
         (lambda x, y: re.sub(r"(\[MASK\]|_+)", y, x, 1)),
-        get_topn(content, tokenizer, model, mask_id, 1),
+        get_topn(content, tokenizer, model, mask_id, 1, stopwords=stopwords),
         content
     )
 
-def get_mlm_message(tokenizer, model, content):
+
+def get_mlm_message(tokenizer, model, mask_id, content, stopwords=None):
     message = ""
-    for i, s in enumerate(get_topn(content, nor_tokenizer, nor_model, nor_mask_id, 5), start=1):
+    for i, s in enumerate(get_topn(content, nor_tokenizer, nor_model, nor_mask_id, 5, stopwords=stopwords), start=1):
         if s:
             message += f"{i}: {s}\n"
     return message
@@ -79,7 +94,7 @@ class Bert(commands.Cog):
         Bert MLM
         """
         content = " ".join(content)
-        result = get_mlm_message(tokenizer, model, content)
+        result = get_mlm_message(tokenizer, model, mask_id, content, stopwords=stopword_ids)
 
         if not message:
             return await no_mask_error(ctx)
@@ -92,7 +107,7 @@ class Bert(commands.Cog):
         Make Bert fill in words marked with [MASK] in sentences
         """
         content = " ".join(content)
-        result = insert(tokenizer, model, content)
+        result = insert(tokenizer, model, mask_id, content, stopwords=stopword_ids)
 
         if not result or result == content:
             return await no_mask_error(ctx)
@@ -114,7 +129,7 @@ class Bert(commands.Cog):
         NorBert MLM
         """
         content = " ".join(content)
-        message = get_mlm_message(nor_tokenizer, nor_model, content)
+        message = get_mlm_message(nor_tokenizer, nor_model, nor_mask_id, content, stopwords=nor_stopword_ids)
 
         if not message:
             return await no_mask_error(ctx)
@@ -127,7 +142,7 @@ class Bert(commands.Cog):
         Make Bert fill in words marked with [MASK] in sentences
         """
         content = " ".join(content)
-        result = insert(nor_tokenizer, nor_model, content)
+        result = insert(nor_tokenizer, nor_model, nor_mask_id, content, stopwords=nor_stopword_ids)
 
         if not result or result == content:
             return await no_mask_error(ctx)
