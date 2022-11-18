0951dab0792d1510f3b04e7b14db4f817223c1ea
LBlend <lblend@protonmail.com>
2022-11-18 17:07:02 +0100

    port misc cog to slash commands

--- src/cogs/misc.py
+++ src/cogs/misc.py
@@ -1,7 +1,8 @@
-from discord.ext import commands
 import discord
+from discord import app_commands
+from discord.ext import commands
 
-from time import time, perf_counter
+from time import time
 import platform
 from os import getpid, environ
 from psutil import Process
@@ -14,36 +15,29 @@ class Misc(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.command(aliases=["info", "about"])
-    async def botinfo(self, ctx):
+    @app_commands.checks.bot_has_permissions(embed_links=True)
+    @app_commands.command()
+    async def botinfo(self, interaction: discord.Interaction):
         """
         Displays key information about the bot
         """
-
         dev = await self.bot.fetch_user(142720616661909504)
         invite_link = "https://discordapp.com/oauth2/authorize?client_id=" + \
                       f"{self.bot.user.id}&permissions=378944&scope=bot"
-
-        # Ping
-        start = perf_counter()
-        status_msg = await ctx.send("Pinging...")
-        end = perf_counter()
-        ping = int((end - start) * 1000)
 
         # Memory usage
         process = Process(getpid())
         memory_usage = round(process.memory_info().rss / 1000000, 1)
 
         # Build embed
-        embed = discord.Embed(color=ctx.me.color, url=self.bot.source_code_url, title=self.bot.user.name)
-        embed.set_author(name=dev.name, icon_url=dev.avatar_url)
-        embed.set_thumbnail(url=self.bot.user.avatar_url)
+        embed = discord.Embed(url=self.bot.source_code_url, title=self.bot.user.name)
+        embed.set_author(name=dev.name, icon_url=dev.avatar)
+        embed.set_thumbnail(url=self.bot.user.avatar)
         embed.add_field(name="Dev", value=f"{dev.mention}\n{dev.name}#{dev.discriminator}", inline=False)
         embed.add_field(name="Uptime", value=await self.get_uptime(), inline=False)
         embed.add_field(
-            name="Ping",
-            value=f"Real: {ping} ms\nWebsocket: {int(self.bot.latency * 1000)} ms",
+            name="Websocket Ping",
+            value=f"{int(self.bot.latency * 1000)} ms",
             inline=False
         )
         embed.add_field(
@@ -65,62 +59,57 @@ class Misc(commands.Cog):
             value=f"[Source code]({self.bot.source_code_url}) | [Invite link]({invite_link})",
             inline=False
         )
-        embed_templates.default_footer(ctx, embed)
-        await status_msg.edit(embed=embed, content=None)
+        embed_templates.default_footer_interaction(interaction, embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.command()
-    async def uptime(self, ctx):
+    @app_commands.checks.bot_has_permissions(embed_links=True)
+    @app_commands.command()
+    async def uptime(self, interaction: discord.Interaction):
         """
         Fetches the amount of time the bot has been running for
         """
-
-        embed = discord.Embed(color=ctx.me.color)
+        embed = discord.Embed(color=interaction.client.user.color)
         embed.add_field(name="🔌 Uptime", value=await self.get_uptime())
-        embed_templates.default_footer(ctx, embed)
-        await ctx.reply(embed=embed)
+        embed_templates.default_footer_interaction(interaction, embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.command()
-    async def version(self, ctx):
+    @app_commands.checks.bot_has_permissions(embed_links=True)
+    @app_commands.command()
+    async def version(self, interaction: discord.Interaction):
         """
         Check version
         """
-
         githash = Repo("../").head.commit
 
-        embed = discord.Embed(color=ctx.me.color, title="Git commit hash")
+        embed = discord.Embed(color=interaction.client.user.color, title="Git commit hash")
         embed.description = f"[{githash}]({self.bot.source_code_url}/commit/{githash})"
-        await ctx.reply(embed=embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.bot_has_permissions(embed_links=True)
-    @commands.command(aliases=["githubrepo", "repo", "git"])
-    async def github(self, ctx):
+    @app_commands.checks.bot_has_permissions(embed_links=True)
+    @app_commands.command()
+    async def github(self, interaction: discord.Interaction):
         """
         Get source code url
         """
-
-        embed = discord.Embed(color=ctx.me.color)
+        embed = discord.Embed(color=interaction.client.user.color)
         embed.add_field(
             name="🔗 Github Repo",
             value=f"[Click here]({self.bot.source_code_url}) to view my source code"
         )
-        embed_templates.default_footer(ctx, embed)
-        await ctx.send(embed=embed)
+        embed_templates.default_footer_interaction(interaction, embed)
+        await interaction.response.send_message(embed=embed)
 
-    @commands.command()
-    async def ping(self, ctx):
+    @app_commands.command()
+    async def ping(self, interaction: discord.Interaction):
         """
         Check if the bot is able to respond
         """
-
-        await ctx.reply("Pong!")
+        await interaction.response.send_message("Pong!")
 
     async def get_uptime(self):
         """
         Returns the current uptime of the bot in string format
         """
-
         now = time()
         diff = int(now - self.bot.uptime)
         days, remainder = divmod(diff, 24 * 60 * 60)
