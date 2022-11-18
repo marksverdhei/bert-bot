63234af0779097ed213b5a2f082545d82c1881fa
LBlend <lblend@protonmail.com>
2022-11-18 17:10:38 +0100

    output slash commands to stdout

--- src/cogs/errors.py
+++ src/cogs/errors.py
@@ -1,3 +1,5 @@
+import discord
+from discord import app_commands
 from discord.ext import commands
 
 import traceback
@@ -15,7 +17,6 @@ class Errors(commands.Cog):
         """
         Prints command execution metadata
         """
-
         print(
             f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} | " +
             f"{'❌ ' if ctx.command_failed else '✔ '} {ctx.command} - " +
@@ -24,7 +25,6 @@ class Errors(commands.Cog):
 
     @commands.Cog.listener()
     async def on_command_error(self, ctx, error):
-
         #  Ignore if command has its own error handling
         if hasattr(ctx.command, "on_error"):
             return
@@ -49,6 +49,44 @@ class Errors(commands.Cog):
 
         #  Print full exception to console
         print(f"Ignoring exception in command {ctx.command}:", file=sys.stderr)
+        traceback.print_exception(
+            type(error),
+            error,
+            error.__traceback__,
+            file=sys.stderr
+        )
+
+    @commands.Cog.listener()
+    async def on_app_command_completion(
+        self,
+        interaction: discord.Interaction,
+        command: app_commands.Command | app_commands.ContextMenu
+    ):
+        """
+        Logs slash command execution metadata
+        Parameters
+        ----------
+        interaction (discord.Interaction): Slash command context object
+        command (app_commands.Command | app_commands.ContextMenu): Command object
+        """
+        print(
+            f"{'❌ ' if interaction.command_failed else '✔ '} {command.name} | " +
+            f"{interaction.user.name}#{interaction.user.discriminator} ({interaction.user.id}) | " +
+            f"{interaction.guild_id}-{interaction.channel_id}-{interaction.id}"
+        )
+
+    async def on_app_command_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        """
+        Handle slash command errors
+        Parameters
+        ----------
+        interaction (discord.Interaction): Slash command context object
+        error (app_commands.AppCommandError): Eror context object
+        """
+        await self.on_app_command_completion(interaction, interaction.command)  # Print command usage, just in case
+
+        #  Print full exception to console
+        print(f"Ignoring exception in command {interaction.command}:", file=sys.stderr)
         traceback.print_exception(
             type(error),
             error,
