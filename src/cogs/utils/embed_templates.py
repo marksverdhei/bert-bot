d2879cb5447ccc41487b1aa891d0cab3d6c58178
LBlend <lblend@protonmail.com>
2022-11-18 17:04:55 +0100

    create default footer for slash commands

--- src/cogs/utils/embed_templates.py
+++ src/cogs/utils/embed_templates.py
@@ -1,2 +1,11 @@
-def default_footer(ctx, embed):
+
+import discord
+from discord.ext import commands
+
+
+def default_footer(ctx: commands.Context, embed: discord.Embed):
     return embed.set_footer(icon_url=ctx.author.avatar_url, text=f"{ctx.author.name}#{ctx.author.discriminator}")
+
+
+def default_footer_interaction(interaction: discord.Interaction, embed: discord.Embed):
+    return embed.set_footer(icon_url=interaction.user.avatar, text=f"{interaction.user.name}#{interaction.user.discriminator}")
